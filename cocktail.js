7d366cd1c53b95a495322bd18a3ee6fba54259c7
Ctucker9233 <court.m.tucker@gmail.com>
2019-10-25 20:34:53 -0700

    Added basic html for Build Your Own Drink page

--- cocktail.js
+++ cocktail.js
@@ -1,14 +1,14 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://the-cocktail-db.p.rapidapi.com/list.php?c=" + drink,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
-		"x-rapidapi-key": "c941fba9d6msh89d2c84a1b6155ap13fc7cjsn95e6f99347b9"
-	}
-}
+// var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://the-cocktail-db.p.rapidapi.com/list.php?c=" + drink,
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+// 		"x-rapidapi-key": "c941fba9d6msh89d2c84a1b6155ap13fc7cjsn95e6f99347b9"
+// 	}
+// }
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
