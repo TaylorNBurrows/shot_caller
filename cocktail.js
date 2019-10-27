28767f24fc9e8356b08c5d619cd72ddc0368bb60
Ctucker9233 <court.m.tucker@gmail.com>
2019-10-27 11:09:34 -0700

    reconfigured cocktail js so that a default drink gets immediately rendered and afterwards any clicked drink will replace the default

--- cocktail.js
+++ cocktail.js
@@ -1,7 +1,12 @@
+//global variables
 var alcoholImg = document.getElementsByClassName("modal-close");
 var alcoholId;
 var drinkId;
 var drinkArray;
+var drinkImage = document.getElementById("drink-image");
+var cardIndex;
+
+//This loops through the images in the modal
 for (i = 0; i < alcoholImg.length; i++) {
 	$(alcoholImg[i]).on("click", function () {
 		alcoholId = this.id;
@@ -16,11 +21,11 @@ for (i = 0; i < alcoholImg.length; i++) {
 			}
 		}
 		console.log(alcoholId);
-		$.ajax(settings).done(function (response) {
-			console.log(response);
-			drinkId = response.drinks[0].idDrink
-			callRecipe(drinkId)
-			listDrinks(response);
+		$.ajax(settings).done(function (intialResponse) {
+			console.log(intialResponse);
+			listDrinks(intialResponse);
+			drinkImg(intialResponse)
+			callRecipe(intialResponse);
 		});
 	})
 }
@@ -32,13 +37,75 @@ function listDrinks(response) {
 	for (i = 0; i < drinkArray.length; i++) {
 		var drinkCard = document.createElement("div");
 		drinkCard.setAttribute("class", "card-panel");
+		drinkCard.setAttribute("data-index", drinkArray[i].idDrink)
 		var drinkText = document.createTextNode(drinkArray[i].strDrink);
 		drinkCard.appendChild(drinkText);
 		drinkList.appendChild(drinkCard);
 	}
 }
 
-function callRecipe(drinkId) {
+var drinkCard = document.querySelectorAll("#drink-choices");
+console.log(drinkCard);
+console.log(drinkCard.length);
+
+for (i = 0; i < drinkCard.length; i++) {
+	console.log(drinkCard.length);
+	drinkCard[i].addEventListener("click", function (event) {
+		event.stopPropagation;
+		console.log(drinkCard[0].children[0].attributes[1].nodeValue)
+		cardIndex = drinkCard[0].children[0].attributes[1].nodeValue;
+		var settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://the-cocktail-db.p.rapidapi.com/filter.php?i=" + cardIndex,
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+				"x-rapidapi-key": "c941fba9d6msh89d2c84a1b6155ap13fc7cjsn95e6f99347b9"
+			}
+		}
+		console.log(cardIndex);
+		$.ajax(settings).done(function (response) {
+			console.log(response);
+			callRecipe(response);
+		});
+		//callRecipe(cardIndex);
+		//alcoholId = this.id;
+		// var settings = {
+		// 	"async": true,
+		// 	"crossDomain": true,
+		// 	"url": "https://the-cocktail-db.p.rapidapi.com/filter.php?i=" + alcoholId,
+		// 	"method": "GET",
+		// 	"headers": {
+		// 		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+		// 		"x-rapidapi-key": "c941fba9d6msh89d2c84a1b6155ap13fc7cjsn95e6f99347b9"
+		// 	}
+		// }
+		// console.log(alcoholId);
+		// $.ajax(settings).done(function (response) {
+		// 	console.log(response);
+		// 	drinkId = response.drinks[0].idDrink
+		// 	imageSource = response.drinks[0].strDrinkThumb
+		// 	callRecipe(drinkId);
+		// 	listDrinks(response);
+		// 	drinkImg(imageSource)
+		// });
+	})
+}
+
+function drinkImg(response) {
+	console.log(response);
+	imageSource = response.drinks[0].strDrinkThumb;
+	console.log(drinkImage.children[0].src);
+	drinkImage.children[0].src = imageSource
+}
+
+function callRecipe(response) {
+	console.log(response)
+	if (typeof response === 'object'){
+		drinkId = response.drinks[0].idDrink
+	}
+	
 	var recipe = {
 		"async": true,
 		"crossDomain": true,
@@ -49,17 +116,18 @@ function callRecipe(drinkId) {
 			"x-rapidapi-key": "c941fba9d6msh89d2c84a1b6155ap13fc7cjsn95e6f99347b9"
 		}
 	}
-
 	$.ajax(recipe).done(function (recipeResponse) {
 		console.log(recipeResponse);
-		drinkRecipe(recipeResponse)
+		drinkRecipe(recipeResponse);
+		drinkImg(recipeResponse);
 	});
 }
+
 function drinkRecipe(drinkResponse) {
 	drinkArray = drinkResponse.drinks
-	var recipeEl = document.getElementById("drink-recipe");
+	var recipeEl = document.getElementById("instructions");
+	$(recipeEl).empty();
 	var ingredientsEl = document.createElement('div');
-
 	var ingredientsArray = [];
 	drinkArray.forEach(drink => {
 		var keys = Object.keys(drink);
@@ -101,15 +169,21 @@ function drinkRecipe(drinkResponse) {
 		ingredientsEl.appendChild(ingredientsText);
 	}
 
-	
 	recipeEl.appendChild(ingredientsEl);
+	var instructions = drinkResponse.drinks[0].strInstructions;
+	var instructionEl = document.createElement("div");
+	var instructionTitle = document.createElement("h3");
+	instructionTitle.innerText = "Instructions:"
+	var instructionText = document.createElement("p");
+	instructionText.innerText = instructions;
+	instructionEl.appendChild(instructionTitle);
+	instructionEl.appendChild(instructionText);
+	recipeEl.appendChild(instructionEl);
+
 }
 
-<<<<<<< HEAD
-=======
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+
+
 
 var random = {
 	"async": true,
@@ -122,19 +196,16 @@ var random = {
 	}
 }
 
-
-
 var generateRandom = document.getElementById("gen-random");
 
-generateRandom.addEventListener("click", function(){
-$.ajax(random).done(function (response) {
-	console.log("random drink", response);
+generateRandom.addEventListener("click", function () {
+	$.ajax(random).done(function (response) {
+		console.log("random drink", response);
 
-	var randomResult = response.drinks[0]
-	console.log(randomResult);
+		var randomResult = response.drinks[0]
+		console.log(randomResult);
 
-});
+	});
 
 })
 
->>>>>>> 1a9762fc4071071f66d1b9b9c98f4f4752a971ca
